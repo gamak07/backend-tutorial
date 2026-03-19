b937ded9da3fb489c01876401707bdaf0691a1d1
gamak07 <ganiyumubarak09@gmail.com>
2026-03-19 22:13:36 +0100

    created the auth middleware, completed the crud apis

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -1,7 +1,7 @@
 import { prisma } from "../config/db.js";
 
 const addToWatchlist = async (req, res) => {
-  const { movieId, status, rating, note, userId } = req.body;
+  const { movieId, status, rating, note } = req.body;
 
   // verify movie exists
   const movie = await prisma.movie.findUnique({
@@ -17,7 +17,7 @@ const addToWatchlist = async (req, res) => {
   const existingItem = await prisma.watchListItem.findUnique({
     where: {
       userId_movieId: {
-        userId: userId,
+        userId: req.user.id,
         movieId: movieId,
       },
     },
@@ -27,10 +27,9 @@ const addToWatchlist = async (req, res) => {
     return res.status(400).json({ message: "Movie already in watchlist" });
   }
 
-
   const watchlist = await prisma.watchListItem.create({
     data: {
-      userId,
+      userId: req.user.id,
       movieId,
       status: status || "PLANNED",
       rating,
@@ -44,4 +43,97 @@ const addToWatchlist = async (req, res) => {
   });
 };
 
-export {addToWatchlist}
+const updateWatchlist = async (req, res) => {
+  // verify movie exists
+  const { id } = req.params;
+  const { status, rating, note } = req.body;
+
+  // verify movie is in watchlist
+  const existingItem = await prisma.watchListItem.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!existingItem) {
+    return res.status(404).json({ message: "Movie not found in watchlist" });
+  }
+
+  // ensure only owner can update
+  if (existingItem.userId !== req.user.id) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+
+  // update watchlist
+  const watchlist = await prisma.watchListItem.update({
+    where: {
+      id,
+    },
+    data: {
+      status,
+      rating,
+      note,
+    },
+  });
+  res.status(200).json({
+    status: "success",
+    data: watchlist,
+  });
+};
+const deleteWatchlist = async (req, res) => {
+  // get the watchlist id from the url
+  const { id } = req.params;
+
+  // verify the watchlist exists
+  const existingItem = await prisma.watchListItem.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!existingItem) {
+    return res.status(404).json({ message: "Movie not found in watchlist" });
+  }
+
+  // verify the user owns the watchlist
+  if (existingItem.userId !== req.user.id) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+
+  // delete the watchlist
+  await prisma.watchListItem.delete({
+    where: {
+      id,
+    },
+  });
+
+  // send response
+  res.status(200).json({
+    status: "success",
+    message: "Movie removed from watchlist",
+  });
+};
+const getWatchlist = async (req, res) => {
+  // get all watchlist data
+
+  const { status, rating } = req.query;
+
+  // get logged in user watchlist
+  const watchlist = await prisma.watchListItem.findMany({
+    where: {
+      userId: req.user.id,
+      ...(status && { status }),
+      ...(rating && { rating }),
+    },
+    include: {
+      movie: true,
+    },
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: watchlist,
+  });
+};
+
+export { addToWatchlist, updateWatchlist, deleteWatchlist, getWatchlist };
--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { addToWatchlist } from "../controllers/watchlistController.js";
+import { addToWatchlist, updateWatchlist, deleteWatchlist, getWatchlist } from "../controllers/watchlistController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
+router.use(authMiddleware)
 
 router.post("/", addToWatchlist);
+router.put("/:id", updateWatchlist);
+router.delete("/:id", deleteWatchlist);
+router.get("/", getWatchlist);
 
 export default router;
